6599f20f2e9b79f3ee6435f33d2d2ba2cdfe4eaf
Kostia Donchenko <kostia.donchenko@gmail.com>
2026-03-03 22:08:14 +0200

    add an meet template on main page

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,7 +9,7 @@ function Main() {
     return (
         <Container>
             <main className="min-h-[calc(100vh-82px)] flex items-center">
-                <div className="flex-2">
+                <div className="flex-4">
                     <div>
                         <h2 className="text-5xl font-bold pb-8">Video calls specifically <br />designed for designers.</h2>
                         <h4 className="text-md text-gray-600 pb-8">Connect, collaborate, and share ideas with premium video quality and <br />distraction-free controls.</h4>
@@ -33,11 +33,24 @@ function Main() {
                                 <Link href="/meeting-code">Join</Link>
                             </Button>
                         </div>
-                        
                     </div>
                 </div>
-                <div className="hidden lg:block flex-1">
-                    imgfdgfd
+                <div className="hidden xl:block flex-3">
+                    <div className="grid grid-rows-2 grid-cols-2 gap-4">
+                        <div className="w-full max-w-full aspect-video rounded-md bg-white border border-gray-200 flex justify-center items-center">
+                            <div className="w-1/4 aspect-square rounded-full bg-orange-200 flex justify-center items-center text-gray-600">DS</div>
+                        </div>
+                         <div className="w-full max-w-full aspect-video rounded-md bg-white border border-gray-200 flex justify-center items-center">
+                            <div className="w-1/4 aspect-square rounded-full bg-amber-200 flex justify-center items-center text-gray-600">FD</div>
+                        </div>
+                         <div className="w-full max-w-full aspect-video rounded-md bg-white border border-gray-200 flex justify-center items-center">
+                            <div className="w-1/4 aspect-square rounded-full bg-green-200 flex justify-center items-center text-gray-600">YU</div>
+                        </div>
+                         <div className="w-full max-w-full aspect-video rounded-md bg-white border border-gray-200 flex justify-center items-center">
+                            <div className="w-1/4 aspect-square rounded-full bg-cyan-200 flex justify-center items-center text-gray-600">KR</div>
+                        </div>
+                        
+                    </div>
                 </div>
             </main>
         </Container>
